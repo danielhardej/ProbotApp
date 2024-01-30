8e7f66bfe7c73b25a11f19d3f309a0b256487893
Dan Hardej <danielhardej@github.com>
2024-01-31 07:42:08 +0800

    Update to add Azure logging

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
-module.exports = (app) => {
-  // Your code here
+module.exports = (app, context) => {
   app.log.info("Yay, the app was loaded!");
   context.log("Yay, the app was loaded!");
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
+  app.on("issues.opened", async (probotContext) => {
+    const issueComment = probotContext.issue({
       body: "Thanks for opening this issue!",
     });
-    const issueAssignees = context.issue({
+    const issueAssignees = probotContext.issue({
       assignees: "danielhardej",
     });
-    context.octokit.issues.addAssignees(issueAssignees);
-    context.octokit.issues.createComment(issueComment);
-    context.log(`Issue opened: ${context.issue}, Assignees added: ${issueAssignees}, Comment created: ${issueComment}`);
+    probotContext.octokit.issues.addAssignees(issueAssignees);
+    probotContext.octokit.issues.createComment(issueComment);
+
+    context.log(`Issue opened: ${probotContext.issue}, Assignees added: ${issueAssignees}, Comment created: ${issueComment}`);
   });
 };
